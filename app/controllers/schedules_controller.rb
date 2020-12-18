5748a5ce90dbd07c6def6a38c485c369c69f38d6
kojima919 <kd1306653@st.kobedenshi.ac.jp>
2020-12-18 14:25:13 +0900

    予定追加のdbに保存するところを書いてみました

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -2,4 +2,17 @@ class SchedulesController < ApplicationController
     def index
         
     end
+    def create
+        @post = Post.new(name: params[:name])
+        @post.save
+        
+        @post = Post.new(staretdate: params[:startdate])
+        @post.save
+        
+        @post = Post.new(moemo: parms[:memo])
+        @post.save
+        redirect_to("")
+
+    end
 end
+
