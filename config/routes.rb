dc4b78cd2abdd9c85db496db5c66142fa2c09464
marina-gen <ishitomochi@gmail.com>
2021-01-10 00:50:34 +0900

    ユーザーログインタスクはまだ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/lodout', to: 'sessions#destroy'
+  namespace :admin do
+    resources :users
+  end
   get 'localhost:3000/main_screen' => 'schedule#index'
   resources :schedules
   root to: 'main_screens#index'
