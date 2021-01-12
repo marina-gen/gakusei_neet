80e854e907e3e34b7c459c938234a956a7dac346
benibenisan <kd1265438@st.kobedenshi.ac.jp>
2021-01-12 10:10:40 +0900

    日記画面(途中まで)です

--- app/controllers/main_diaries_controller.rb
+++ app/controllers/main_diaries_controller.rb
@@ -1,4 +1,19 @@
 class MainDiariesController < ApplicationController
-  def create
+  def index
+    @diaries = MainDiary.all
+    
   end
+  
+  def new
+    @diary = MainDiary.new
+  end
+  
+  def create
+    @diaries = MainDiary.new(diary: params[:diary],today: params[:today])            
+    @diaries.save
+    redirect_to('/main_diaries/index')
+  end
+
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'main_diaries/create' => "main_diaries#create"
- 
+  get 'main_diaries/index' => "main_diaries#index"
+  get 'main_diaries/new' => "main_diaries#new"
+  post 'main_diaries/create' => "main_diaries#create"
+
 end
