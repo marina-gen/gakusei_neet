45324b51ee974113c8ebc1c91847133ba3dcc905
masa3889 <kd1309884@st.kobedenshi.ac.jp>
2021-01-13 13:27:24 +0900

    できたところまで

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -3,8 +3,8 @@ class CalendarsController < ApplicationController
     d = DateTime.current.beginning_of_day  # 今日の0:00
     nd = d + 1  # 明日
     
-
+    @schedules = Schedule.all
     # 今日の予定を取得
-    @schedules = Schedule.where("startdate < ? AND enddate >= ?", params[:nd], params[:d]).order(startdate: :asc)
+    @slists = Schedule.where("startdate < ? AND enddate >= ?", params[:nd], params[:d]).order(startdate: :asc)
   end
 end
