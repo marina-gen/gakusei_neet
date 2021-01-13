429d5ba6f469030a2d4cfb72e354907edbd37946
marina-gen <ishitomochi@gmail.com>
2021-01-14 04:07:18 +0900

    コントローラー修正view異常あり

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,17 +1,32 @@
 class SchedulesController < ApplicationController
+    
+    def new
+        @schedule = Schedule.new
+    end
+    
     def index
+        #@schedule = Schedules.all
         
     end
     def create
-        schedule = Schedules.new(schedule_params)
+        @schedule = Schedules.new(schedule_params)
     
-        schedule.save!
-        redirect_to("")#リダイレクト先URL
+        if @schedule.save
+            render 'index'
+        else
+            render 'new'
+        end
+
+        flash[:notice] = "スケジュール「#{schedule.name}」登録しました。"
+        redirect_to action: :index
+        
 
     end
 
-    def schedule_params
-        params.require(:schedule).permit(:name, :startdate, :memo)
-    end
+    private
+
+        def schedule_params
+            params.require(:schedule).permit(:name, :description)
+        end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :schedules# , only: [:create, :new]
+  resources :schedules 
 end
+
 
