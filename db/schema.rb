46c39b0de6719b96a0cdeb68d49faece73057c42
marina-gen <ishitomochi@gmail.com>
2021-01-13 14:37:41 +0900

    fix_login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_08_182233) do
+ActiveRecord::Schema.define(version: 2021_01_10_203742) do
 
   create_table "schedules", force: :cascade do |t|
     t.string "name"
